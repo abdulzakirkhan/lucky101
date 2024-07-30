ba881b20fc839da786e72967f3639e1e1bb33748
Abdul Zakir <abdulzakir632@gmail.com>
2024-07-30 15:47:45 +0500

    changes

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import cookieParser from "cookie-parser";
-import { app } from "./socket/socket.js";
+import cors from "cors";
+import { app } from "./socket/socket.js"; // Assuming app is being exported from this file
 
 dotenv.config();
 
+// Connect to MongoDB
 mongoose
   .connect(
     `mongodb+srv://abdulzakir632:zakirnayab143@cluster0.gz21zfv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
@@ -18,27 +20,33 @@ mongoose
   });
 
 const PORT = process.env.PORT || 300;
+
+// Middleware setup
+app.use(cors()); // Enable CORS for all routes
 app.use(express.json());
 app.use(cookieParser());
 
+// Test route
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
-// import routes
+// Import routes
 import authRoutes from "./routes/auth.routes.js";
 // import messageRoute from "./routes/message.routes.js";
 import userRoute from "./routes/user.routes.js";
 
+// Use routes
 app.use("/api/auth", authRoutes);
 // app.use("/api/messages", messageRoute);
 app.use("/api/users", userRoute);
 
+// Start the server
 app.listen(PORT, () => {
   console.log("Server is running on port " + PORT);
 });
 
-// error handler
+// Error handler
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || "Internal Server Error";
@@ -49,3 +57,6 @@ app.use((err, req, res, next) => {
     message,
   });
 });
+
+
+
